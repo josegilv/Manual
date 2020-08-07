5da65a0ec1c7855467d983d7d1753afd0ec81e0d
Jose Maria Gil <josegilv@gmail.com>
2020-08-07 16:48:04 -0500

    Se hace la modificación inicial

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        int edad;
+
+        public void Main ()
+        {
+            Console.WriteLine("Ingrese la edad:");
+        }
+        
+        
+    }
+}
